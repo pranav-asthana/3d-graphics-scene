8106dae3a1b8714621b62f025067bd09174b76b7
Pranav Asthana (Partner) <pranav.i.asthana@happiestminds.com>
2017-10-26 15:24:03 +0530

    got camera working. Use mouse and arrow keys

--- 3d.cpp
+++ 3d.cpp
@@ -17,42 +17,99 @@
 
 float xpan = 0.0f;
 float ypan = 0.0f;
+
+float deltaTime = 0.0f;	// Time between current frame and last frame
+float lastFrame = 0.0f; // Time of last frame
+
 glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
+
+bool firstMouse = true;
+float yaw   = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch =  0.0f;
+float lastX =  800.0f / 2.0;
+float lastY =  600.0 / 2.0;
+float fov   =  45.0f;
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 0.1f; // change this value to your liking
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    // make sure that when pitch is out of bounds, screen doesn't get flipped
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(front);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    if (fov >= 1.0f && fov <= 45.0f)
+        fov -= yoffset;
+    if (fov <= 1.0f)
+        fov = 1.0f;
+    if (fov >= 45.0f)
+        fov = 45.0f;
+}
 
 void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 
-    // float cameraSpeed = 0.05f; // adjust accordingly
-    // if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-    //     cameraPos += cameraSpeed * cameraFront;
-    // if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-    //     cameraPos -= cameraSpeed * cameraFront;
-    // if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-    //     cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
-    // if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-    //     cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    float cameraSpeed = 2.5f * deltaTime; // adjust accordingly
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 
-    if(glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-    {
-        xpan += 0.01;
-        // if (xpan > 1.0)
-        //     xpan = 1.0;
-    }
-    if(glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-    {
-        xpan -= 0.01;
-        // if (xpan < -1.0)
-        //     xpan = -1.0;
-    }
-    if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-        ypan += 0.01;
-
-    if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-        ypan -= 0.01;
+    // if(glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+    // {
+    //     xpan += 0.01;
+    //     // if (xpan > 1.0)
+    //     //     xpan = 1.0;
+    // }
+    // if(glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+    // {
+    //     xpan -= 0.01;
+    //     // if (xpan < -1.0)
+    //     //     xpan = -1.0;
+    // }
+    // if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+    //     ypan += 0.01;
+    //
+    // if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+    //     ypan -= 0.01;
 }
 
 void drawCube(GLfloat vertices[])
@@ -125,9 +182,15 @@ int main()
     // Make the window's context
     glfwMakeContextCurrent(window);
 
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+    // tell GLFW to capture our mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
     glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT);
-    // glMatrixMode(GL_PROJECTION);
-    // glLoadIdentity();
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective((float)fov, (float)SCREEN_WIDTH/(float)SCREEN_HEIGHT, 0.1f, 100.0f);
     // glMatrixMode(GL_MODELVIEW);
     // glLoadIdentity();
 
@@ -235,22 +298,25 @@ int main()
         -0.5f,  0.5f, -0.5f
     };
 
-
     glm::vec3 cubePositions[] = {
-            glm::vec3( 0.0f,  0.0f,  0.0f),
-            glm::vec3( 0.5f,  0.0f,  -0.5f),
-            glm::vec3( 0.5f,  0.0f,  0.5f),
-            glm::vec3(-0.5f,  0.0f, -0.3f),
-            glm::vec3(-0.5f,  0.0f,  0.5f),
-            glm::vec3(-1.7f,  3.0f, -7.5f),
-            glm::vec3( 1.3f, -2.0f, -2.5f),
-            glm::vec3( 1.5f,  2.0f, -2.5f),
-            glm::vec3( 1.5f,  0.2f, -1.5f),
-            glm::vec3(-1.3f,  1.0f, -1.5f)
-        };
+        glm::vec3( 2.0f,  0.0f, -0.0f),
+        glm::vec3( 2.0f,  0.0f, -5.0f),
+        glm::vec3( 2.0f,  0.0f, -10.0f),
+        glm::vec3( 2.0f,  0.0f, -15.0f),
+        glm::vec3( 2.0f,  0.0f, -20.0f),
+        glm::vec3(-2.0f,  0.0f, 0.0f),
+        glm::vec3(-2.0f,  0.0f, -5.0f),
+        glm::vec3(-2.0f,  0.0f, -10.0f),
+        glm::vec3(-2.0f,  0.0f, -15.0f),
+        glm::vec3(-2.0f,  0.0f, -20.0f),
+    };
 
     int rotation = 0;
     while (!glfwWindowShouldClose(window)) {
+
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
 
         processInput(window);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -292,30 +358,30 @@ int main()
 
             glm::mat4 view;
             glm::mat4 projection;
-            projection = glm::perspective(glm::radians(45.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 1000.0f);
+            // projection = glm::perspective(glm::radians(45.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 1000.0f);
             // view       = glm::translate(view, glm::vec3(xpan, ypan, -3.0f));
 
             float radius = 0.1f;
             float camX = sin(xpan) * radius;
             float camZ = cos(xpan) * radius;
-            view = glm::lookAt(glm::vec3(camX, ypan, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+            // view = glm::lookAt(glm::vec3(camX, ypan, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
             // printmatrix(projection);
 
 
-            // view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+            view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 
 
             // calculate the model matrix for each object and pass it to shader before drawing
             glm::mat4 model;
             model = glm::translate(model, cubePositions[i]);
-            // float angle = 20.0f * i;
+            float angle = 20.0f * i;
             // model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 
             // rotation++;
             // model = glm::rotate(model, (float)((3.14+rotation/4)/180), glm::vec3(1.f, 0.f, 0.0f));
 
             glm::mat4 mvp = projection*view*model;
-            mvp = glm::scale(mvp, glm::vec3(0.1, 0.1, 0.1));
+            // mvp = glm::scale(mvp, glm::vec3(0.1, 0.1, 0.1));
             updateVertices(vertices, mvp, 4*6*3);
 
             drawCube(vertices);
